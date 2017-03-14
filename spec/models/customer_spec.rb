3d2a4d8e5f640a77c78d6b8a76674d060089d5f2
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-13 18:43:58 -0600

    Add model validations for relationships

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
+  it { should have_many(:invoices) }
 end
