8a083c922929891888f27391ddcd4059c59d55a2
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-14 10:48:30 -0600

    Add model validations for invoice-items and relationships

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,5 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  validates :item_id, :invoice_id, :quantity, :unit_price, presence: true
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Invoice, type: :model do
   it { should belong_to(:customer) }
   it { should belong_to (:merchant) }
   it { should validate_presence_of(:status) }
+  it { should have_many(:invoice_items) }
+  it { should have_many(:items) }
 end
