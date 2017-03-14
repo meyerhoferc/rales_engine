99d097ada85b59298fc1904c24ae552755e6ce7b
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-14 14:18:38 -0600

    Add unit price formatting

--- app/controllers/api/v1/items/finder_controller.rb
+++ app/controllers/api/v1/items/finder_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::Items::FinderController < ApplicationController
+  before_action :format_unit_price, only: [:index, :show]
+
   def show
     finder = params.keys[0]
     render json: Item.find_by(finder => params[finder])
@@ -11,5 +13,12 @@ class Api::V1::Items::FinderController < ApplicationController
 
   def random
     render json: Item.all.sample
+  end
+
+  def format_unit_price
+    if params[:unit_price]
+      unit_price = params[:unit_price].split(".").join
+      params[:unit_price] = unit_price
+    end
   end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,7 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :unit_price, :merchant_id
 
-  def unit_price
-    object.unit_price.to_i.to_s.chars.insert(-3, ".").join
+  attribute :unit_price do
+    (object.unit_price / 100).round(2).to_s
   end
 end
