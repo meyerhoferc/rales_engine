06a36899485e9e0731679d57478014b4cb7a2532
Spencer Carter <scmountain@gmail.com>
2017-03-15 15:16:08 -0600

    All relationships pass spec harness

--- app/controllers/api/v1/invoice_items/item_controller.rb
+++ app/controllers/api/v1/invoice_items/item_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::InvoiceItems::ItemController < ApplicationController
   def show
-    render json: InvoiceItem.find_by(params[:invoice_id]).item
+    render json: InvoiceItem.find_by(id: item_params[:invoice_item_id]).item
+  end
 
+  def item_params
+    params.permit(:invoice_item_id)
   end
 end
