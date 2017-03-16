5863e5935fbeb8847018471ac05b7a47e9a361e7
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-16 11:08:18 -0600

    Business intelligence merchants (#32)
    * Add ids to seeding and fix find_by timestamp
    
    * Add total revenue across all merchants for a date
    
    * Fix test for customers with pending transactions
    
    * Add simplecov
    
    * Add find_by_sql query for finding customers with pending

--- spec/requests/api/v1/business_intelligence/merchant_request_spec.rb
+++ spec/requests/api/v1/business_intelligence/merchant_request_spec.rb
@@ -59,7 +59,7 @@ describe "Merchant API" do
     expect(customer["id"]).to eq(customer_one.id)
   end
 
-  xit "returns a collection of customers with pending transactions" do
+  it "returns a collection of customers with pending transactions" do
     customer_one, customer_two, customer_three = Fabricate.times(3, :customer)
     merchant = Fabricate(:merchant)
     customer_one_invoices = Fabricate.times(5, :invoice, customer: customer_one, merchant: merchant)
@@ -91,12 +91,39 @@ describe "Merchant API" do
     expect(response).to be_success
     customers = JSON.parse(response.body)
     expect(customers.count).to eq(1)
-    ids = customers.map { |customer| customer["id"]}
+    expect(customers.first["id"]).to eq(customer_one.id)
+  end
 
-    ids.each do |id|
-      expect(id).to_not eq(customer_three.id)
-      expect(id).to_not eq(customer_two.id)
+  it "returns total revenue for date for all merchants" do
+    date_one = "2012-03-16 11:55:05"
+    date_two = "2012-03-07 10:54:55"
+
+    item_one = Fabricate(:item, unit_price: 100)
+    item_two = Fabricate(:item, unit_price: 300)
+    invoices_date_one = Fabricate.times(2, :invoice, created_at: date_one, updated_at: date_one)
+    invoices_date_two = Fabricate.times(2, :invoice, created_at: date_two, updated_at: date_two)
+
+    invoices_date_one.each do |invoice|
+      Fabricate(:invoice_item, invoice: invoice, quantity: 3, unit_price: 100, item: item_one)
+      Fabricate(:transaction, invoice: invoice)
     end
+
+    invoices_date_two.each do |invoice|
+      Fabricate(:invoice_item, invoice: invoice, quantity: 3, unit_price: 300, item: item_two)
+      Fabricate(:transaction, invoice: invoice)
+    end
+
+    get "/api/v1/merchants/revenue?date=#{date_one}"
+
+    expect(response).to be_success
+    total_revenue = JSON.parse(response.body)
+    expect(total_revenue["total_revenue"]).to eq("6.0")
+
+    get "/api/v1/merchants/revenue?date=#{date_two}"
+
+    expect(response).to be_success
+    total_revenue = JSON.parse(response.body)
+    expect(total_revenue["total_revenue"]).to eq("18.0")
   end
 
   it "returns total revenue for date for all merchants" do
