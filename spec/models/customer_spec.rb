e7f499a6964f22b1e8247c32b8f620ed84c6e7ef
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-16 14:15:34 -0600

    Add customer.transactions unit test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Customer, type: :model do
     Fabricate.times(3, :customer)
     expect(Customer.random.class).to eq(Customer)
   end
+
+  it ".transactions returns all transactions for a customer" do
+    customer = Fabricate(:customer)
+    invoice_one, invoice_two, invoice_three = Fabricate.times(3, :invoice, customer: customer)
+    Fabricate(:invoice_item, invoice: invoice_one)
+    Fabricate(:invoice_item, invoice: invoice_two)
+    Fabricate(:invoice_item, invoice: invoice_three)
+    expected_transactions = []
+    expected_transactions << Fabricate(:transaction, invoice: invoice_one)
+    expected_transactions << Fabricate(:transaction, invoice: invoice_two)
+    expected_transactions << Fabricate(:transaction, invoice: invoice_three)
+
+    transactions = customer.transactions
+    expect(transactions[0]).to eq(expected_transactions[0])
+    expect(transactions[1]).to eq(expected_transactions[1])
+    expect(transactions[2]).to eq(expected_transactions[2])
+  end
 end
