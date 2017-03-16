0df583a28a1a38402ac359598e616c93c8a8d3a2
Spencer Carter <scmountain@gmail.com>
2017-03-16 17:29:32 -0600

    adds best day per item

--- spec/requests/api/v1/business_intelligence/items_request_spec.rb
+++ spec/requests/api/v1/business_intelligence/items_request_spec.rb
@@ -34,6 +34,40 @@ describe "Items API" do
     expect(items.first["id"]).to eq(item_one.id)
     expect(items.last["id"]).to eq(item_three.id)
     expect(items.first["id"]).to_not eq(item_two.id)
-
   end
+
+    it " returns the date with the most sales that item.
+     If there are multiple days with equal number of sales,
+     return the most recent day" do
+
+     date_one = "2012-03-16T11:55:05.000Z"
+     date_two = "2012-03-07 10:54:55"
+
+     merchant_one = Fabricate(:merchant)
+
+     item_one = Fabricate(:item, unit_price: 100)
+
+     invoices_date_one = Fabricate.times(2, :invoice, merchant: merchant_one, created_at: date_one, updated_at: date_one)
+     invoices_date_two = Fabricate.times(2, :invoice, merchant: merchant_one, created_at: date_two, updated_at: date_two)
+
+     invoices_date_one.each do |invoice|
+       Fabricate(:invoice_item, invoice: invoice, quantity: 3, item: item_one)
+       Fabricate(:transaction, invoice: invoice)
+     end
+
+     invoices_date_two.each do |invoice|
+       Fabricate(:invoice_item, invoice: invoice, quantity: 1, item: item_one)
+       Fabricate(:transaction, invoice: invoice)
+     end
+
+     get "/api/v1/items/#{item_one.id}/best_day"
+
+     expect(response).to be_success
+
+     date = JSON.parse(response.body)
+     expect(date["best_day"]).to eq(date_one)
+    #  assert_equal "2012-03-16 11:55:05", date
+    #  assert_equal "2012-03-07 10:54:55", best_day_two["best_day"]
+
+ end
 end
